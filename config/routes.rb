b423fb4e9a22b8d7321e59226f629e1d4c993983
Alan Costa Facchini <costafacchini@gmail.com>
2022-02-22 23:01:04 -0300

    Remove unnecessary route and controller for transactions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   root 'stores#index'
 
   resources :stores, only: :index
-  resources :transactions, only: :index
   resources :importations, only: [:new, :create]
 end
